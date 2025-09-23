52b94ab5f0c8a3c322a4992f5fc0d8f6ce94f532
trulsostvedt <148589458+trulsostvedt@users.noreply.github.com>
2025-09-23 21:56:37 +0100

    Update best model parameters and improve hyperparameter tuning in training script

--- Regression/train_regression.py
+++ Regression/train_regression.py
@@ -16,12 +16,12 @@ import os
 print("Current working directory:", os.getcwd())
 
 
-# --- 1) Data ---
+# Data 
 base = Path(__file__).resolve().parent  # path to this script
 X = np.load(base / "X_train.npy")   # shape (700, 6)
 y = np.load(base / "Y_train.npy")   # shape (700,)
 
-# --- 2) Pipeline + hyperparametre ---
+# Pipeline og hyperparametre 
 pipe = Pipeline([
     ("poly",   PolynomialFeatures(include_bias=False)),
     ("scaler", StandardScaler()),
@@ -29,22 +29,22 @@ pipe = Pipeline([
 ])
 
 param_grid = {
-    "poly__degree": [2, 3, 4],                # prøv polynomgrad 2–4
-    "ridge__alpha": [1e-3, 1e-2, 1e-1, 1, 10, 100],  # regulariseringsstyrke
+    "poly__degree": [2, 3, 4], #[2, 3, 4, 5, 6],  prøvde polynomgrad 2–6, både 4 og 5 ga nesten likt resultat på CV R^2, så 4 er nok bedre da den er lavere risiko for overfitting
+    "ridge__alpha": [1e-4 ,1e-3, 1e-2, 1e-1, 1, 10, 100, 1000, 10000],  # regulariseringsstyrke
 }
 
 cv = KFold(n_splits=5, shuffle=True, random_state=42)
 gs = GridSearchCV(pipe, param_grid, cv=cv, scoring="r2", refit=True, n_jobs=-1)
 
-print("Running GridSearchCV (kan ta litt tid)...")
+print("Running GridSearchCV...")
 gs.fit(X, y)
 print("Best params:", gs.best_params_, " | CV R^2:", gs.best_score_)
 
-# --- 3) Tren beste modell på hele datasettet ---
+# Trener beste modell på hele datasettet ---
 best_model = gs.best_estimator_
 best_model.fit(X, y)
-
-# --- 4) Lagre modell og metadata ---
+p
+# Lagrer modell og metadata ---
 joblib.dump(best_model, base / "best_model.pkl")
 with open(base / "best_model_info.json", "w") as f:
     json.dump({
